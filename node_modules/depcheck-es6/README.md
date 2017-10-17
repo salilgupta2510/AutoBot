0dccc876d1ed8b7e23f3a65b9bb41e185a891a86
Salil <g.salil@live.in>
2017-10-17 12:01:32 +0530

    first commit

---
+++ node_modules/depcheck-es6/README.md
@@ -0,0 +1,3 @@
+# This fork has merged back to [depcheck](https://github.com/depcheck/depcheck)!
+
+I will continue to develop and maintain it with [@rumpl](https://github.com/rumpl). Thanks for supports!
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=MerckAutoResponse-Web-Deploy.pubxml -p:Password=XgwhWMsc1QD2459qgjB6oPwbTMb4yopftpvnRro9iD3A9AblKomk0A7rbjWX
+
