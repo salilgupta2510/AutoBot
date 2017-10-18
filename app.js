06f2712532b06277deb44ce15c44d48f4474f742
Salil <g.salil@live.in>
2017-10-18 12:39:50 +0530

    conditions

--- app.js
+++ app.js
@@ -18,5 +18,6 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    if(session.message.text === 'hello world'){
+    session.send("You said: %s", session.message.text);}
 });
